17f433fc7bd8e532483ecd34dd28b96ceb42122f
Edgar-De-Jesus-06 <edgarb.dejesus@gmail.com>
2026-08-10 14:42:46 +0800

    add a header for navigation

---
+++ app/view/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../public/assets/style/input.css">
+    <link rel="stylesheet" href="../public/assets/style/header/navigation.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js">
+    <title>De Jesus Portfolio</title>
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg p-0 ms-5 me-5">
+            <div class="container-fluid p-3">
+                <h1 class="navbar-brand fw-medium fs-6 border rounded-5" style="padding: 0.4em; box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;">
+                    <b class="text-success me-1">●</b>Available For a New Project
+                </h1>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-4">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link navigation fw-medium me-5">Education</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link navigation fw-medium me-5">Projects</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link navigation fw-medium me-5">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link navigation fw-medium me-5">Contact</a>
+                    </li>
+                </ul>
+                <button class="btn btn-dark text-light fw-medium" type="button">Let's Talk<i class="bi bi-arrow-return-left ms-2"></i></button>
+            </div>   
+        </nav>
+    </header>
+</body>
+</html>
