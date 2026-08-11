a45a623baa7a0ae2470d4934390fb5ec69ae8cce
Edgar-De-Jesus-06 <edgarb.dejesus@gmail.com>
2026-08-11 14:55:08 +0800

    added new file

---
+++ app/view/contact.php
@@ -0,0 +1,35 @@
+<div class="container-fluid p-0 mt-5 d-flex justify-content-between">
+    <div class="container p-5 d-flex justify-content-center">
+        <img src="/app/public/assets/images/ContactImage.png" alt="Just Me Working" height="500px" width="500px">
+    </div>
+    <div class="container p-5">
+        <div class="container p-0 mb-3 w-75">
+            <h5 class="fw-bold">Looks Like You Need Help!!!</h5>
+            <h1 class="fw-bold">Reach Out!</h1>
+            <p class="text-secondary fw-light">
+                I usually respond within an hour to messages about projects, collaborations, freelance opportunities,
+                or anything else you'd like to discuss. Don't hesitate to get in touch!
+            </p>
+        </div>
+        <div class="container p-0 w-75">
+            <label for="name" class="form-label text-dark fw-medium">Name</label>
+            <div class="input-group mb-2">
+                <span class="input-group-text text-secondary"><i class="bi bi-person-circle"></i></span>
+                <input type="text" class="form-control bg-light" placeholder="John Paul" name="name" id="name">
+            </div>
+            <label for="email" class="form-label text-dark fw-medium">Email</label>
+            <div class="input-group mb-2">
+                <span class="input-group-text text-secondary"><i class="bi bi-envelope-at"></i></span>
+                <input type="email" class="form-control bg-light" placeholder="johnpaul@gmail.com" name="email" id="email">
+            </div>
+            <label for="message" class="form-label text-dark fw-medium">Message</label>
+            <div class="input-group mb-3">
+                <span class="input-group-text text-secondary"><i class="bi bi-envelope-arrow-up-fill"></i></span>
+                <textarea class="form-control bg-light" placeholder="What's the issue?"></textarea>
+            </div>
+            <div class="container p-0">
+                <button type="submit" class="btn btn-dark text-light fw-medium w-100 rounded-5" name="SendMessage">Sent<i class="bi bi-send ms-2"></i></button>
+            </div>
+        </div>
+    </div>
+</div>
